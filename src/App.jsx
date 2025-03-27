3b6d96968fb1c997d248a0e6495921ac21bf7ed8
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-27 13:40:07 -0700

    set up weather service

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-const App = () => {
+import * as weatherService from './services/weatherService';
 
+const App = () => {
+  const fetchData = async () => {
+    const data = await weatherService.show('New York');
+    console.log(data);
+  }
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
   );
 }
 
