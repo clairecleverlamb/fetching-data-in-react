8a585c43b071ab708d3870a16b182703d7d820b5
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-27 14:06:09 -0700

    add weather search

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import * as weatherService from './services/weatherService';
+import WeatherSearch from './components/WeatherSearch/WeatherSearch';
+import './App.css';
 
 const App = () => {
+  const [weather, setWeather] = useState({});
+
   const fetchData = async () => {
     const data = await weatherService.show('New York');
-    console.log(data);
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    }
+    setWeather(newWeatherState);
   }
   return (
     <main>
       <h1>Weather API</h1>
+      <WeatherSearch />
       <button onClick={fetchData}>Fetch Weather Data</button>
     </main>
   );
 }
 
-export default App
+export default App;
