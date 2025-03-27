ec55491e9c9e0be631b8dd42e608242072a31d5a
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-27 15:04:20 -0700

    update weather details

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { useState } from 'react';
 import * as weatherService from './services/weatherService';
 import WeatherSearch from './components/WeatherSearch/WeatherSearch';
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
+
 import './App.css';
 
 const App = () => {
   const [weather, setWeather] = useState({});
 
-  const fetchData = async () => {
-    const data = await weatherService.show('New York');
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
     const newWeatherState = {
       location: data.location.name,
       temperature: data.current.temp_f,
       condition: data.current.condition.text,
     }
     setWeather(newWeatherState);
-  }
+  };
+  console.log('State: ', weather);
+
   return (
     <main>
       <h1>Weather API</h1>
-      <WeatherSearch />
-      <button onClick={fetchData}>Fetch Weather Data</button>
+      <WeatherSearch fetchData={fetchData} /> 
+      <WeatherDetails weather={weather} />
     </main>
   );
 }
